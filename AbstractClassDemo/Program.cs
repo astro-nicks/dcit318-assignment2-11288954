52d63d427c2b22f465c9a4fd28cc3b7a68809b9a
astro-nicks <astronix05@gmail.com>
2025-08-09 14:51:18 +0000

    Assignment 2

---
+++ AbstractClassDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+    public Circle(double radius) => Radius = radius;
+
+    public override double GetArea() => Math.PI * Radius * Radius;
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea() => Width * Height;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+    }
+}
